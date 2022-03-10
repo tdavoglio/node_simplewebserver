1ac26def3bffbb3d2e57f74b6d70e32fcbd30f15
Thiago Davoglio <thiagodavoglio1@gmail.com>
2022-03-09 23:00:42 -0300

    Push the webserver code

---
+++ index.js
@@ -0,0 +1,9 @@
+process.title = "WebServer";
+
+var args = process.argv, 
+    port = args[2] || 7070,
+    webServer = require('./server');
+
+webServer.listen(port, () => {
+    console.log('Server started at port ' + port);
+})
